9a50f4e9792bafdf5f1d5835df1a7820ffbe1f56
Espen Scheuer <espenscheuer@microsoft.com>
2021-12-19 20:40:11 -0800

    limit testing

--- api/data.py
+++ api/data.py
@@ -6,6 +6,9 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
+    degree = str(self.headers.get('degree'))
+    major = str(self.headers.get('major'))
+    university = str(self.headers.get('university'))
     # return some string here or whatever 
-    self.wfile.write(str('You are going to be sooooo broke XD').encode())
+    self.wfile.write(str('You are going to be sooooo broke after graduating from ' + university + " with a " + degree + " in " + major + " XD").encode())
     return
